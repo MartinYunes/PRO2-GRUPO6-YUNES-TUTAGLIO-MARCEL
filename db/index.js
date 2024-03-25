1733607f8704aec665ec07acf56ce4e1b6421a0b
Juan Tutaglio <JuanTutaglio@MacBook-Pro-de-Mauge.local>
2024-03-25 19:43:06 -0300

    base de dato y imagenef profile

--- db/index.js
+++ db/index.js
@@ -6,7 +6,241 @@ let datos = {
     contraseña: "hola",
     fecha_nacimiento: 22/02/2024,
     documento: 12345678,
-    foto: "/images/users/default-image.png",
+    foto: "/images/users/nacho.png",
     },
 
+
+
+    productos:[
+        {
+        id: 1 ,
+        nombre: "Volkswagen Golf GTI",
+        descripcion: "Golf GTI 2021 30.000km",
+        foto: "ha",
+        comentarios: [
+            comentario_1 = {
+                nombre: "Enzo",
+                comentario: "Muy bueno",
+                imagen_perfil: "/images/users/enzodiaz.png"
+            },
+            comentario_2 = {
+                nombre: "borja",
+                comentario: "Muy malo",
+                imagen_perfil: "/images/users/borja.png"
+            },
+            comentario_3 = {
+                nombre: "nacho",
+                comentario: "Alto auto",
+                imagen_perfil: "/images/users/ponzio.jpeg"
+            }]
+        },
+        
+        {
+            id: 2,
+            nombre: "Porsche 911",
+            descripcion: "911 Carrera S 2022 10.000km",
+            foto: "foto2",
+            comentarios: [
+                {
+                    nombre: "Enzo",
+                    comentario: "Me encanta este auto",
+                    imagen_perfil: "/images/users/enzodiaz.png"
+                },
+                {
+                    nombre: "borja",
+                    comentario: "Una belleza",
+                    imagen_perfil: "/images/users/borja.png"
+                },
+                {
+                    nombre: "nacho",
+                    comentario: "Excelente elección",
+                    imagen_perfil: "/images/users/ponzio.jpeg"
+                }
+            ]
+        },
+        {
+            id: 3,
+            nombre: "Ford Mustang GT",
+            descripcion: "Mustang GT 2023 20.000km",
+            foto: "foto3",
+            comentarios: [
+                {
+                    nombre: "Enzo",
+                    comentario: "Potencia pura",
+                    imagen_perfil: "/images/users/enzodiaz.png"
+                },
+                {
+                    nombre: "borja",
+                    comentario: "Un ícono",
+                    imagen_perfil: "/images/users/borja.png"
+                },
+                {
+                    nombre: "nacho",
+                    comentario: "Sueño hecho realidad",
+                    imagen_perfil: "/images/users/ponzio.jpeg"
+                }
+            ]
+        },
+        {
+            id: 4,
+            nombre: "Chevrolet Camaro SS",
+            descripcion: "Camaro SS 2022 15.000km",
+            foto: "foto4",
+            comentarios: [
+                {
+                    nombre: "Enzo",
+                    comentario: "Rugido impresionante",
+                    imagen_perfil: "/images/users/enzodiaz.png"
+                },
+                {
+                    nombre: "borja",
+                    comentario: "Un clásico moderno",
+                    imagen_perfil: "/images/users/borja.png"
+                },
+                {
+                    nombre: "nacho",
+                    comentario: "Increíble máquina",
+                    imagen_perfil: "/images/users/ponzio.jpeg"
+                }
+            ]
+        },
+        {
+            id: 5,
+            nombre: "BMW M3",
+            descripcion: "M3 Competition 2021 12.000km",
+            foto: "foto5",
+            comentarios: [
+                {
+                    nombre: "Enzo",
+                    comentario: "Elegancia y potencia",
+                    imagen_perfil: "/images/users/enzodiaz.png"
+                },
+                {
+                    nombre: "borja",
+                    comentario: "BMW siempre impresionante",
+                    imagen_perfil: "/images/users/borja.png"
+                },
+                {
+                    nombre: "nacho",
+                    comentario: "Una bestia en la carretera",
+                    imagen_perfil: "/images/users/ponzio.jpeg"
+                }
+            ]
+        },
+        {
+            id: 6,
+            nombre: "Audi R8",
+            descripcion: "R8 V10 Plus 2022 8.000km",
+            foto: "foto6",
+            comentarios: [
+                {
+                    nombre: "Enzo",
+                    comentario: "Futurista y emocionante",
+                    imagen_perfil: "/images/users/enzodiaz.png"
+                },
+                {
+                    nombre: "borja",
+                    comentario: "Un símbolo de rendimiento",
+                    imagen_perfil: "/images/users/borja.png"
+                },
+                {
+                    nombre: "nacho",
+                    comentario: "Increíble sonido de escape",
+                    imagen_perfil: "/images/users/ponzio.jpeg"
+                }
+            ]
+        },
+        {
+            id: 7,
+            nombre: "Mercedes-AMG GT",
+            descripcion: "AMG GT R 2023 18.000km",
+            foto: "foto7",
+            comentarios: [
+                {
+                    nombre: "Enzo",
+                    comentario: "Puro poder germano",
+                    imagen_perfil: "/images/users/enzodiaz.png"
+                },
+                {
+                    nombre: "borja",
+                    comentario: "Mercedes siempre impresionante",
+                    imagen_perfil: "/images/users/borja.png"
+                },
+                {
+                    nombre: "nacho",
+                    comentario: "Un rugido intimidante",
+                    imagen_perfil: "/images/users/ponzio.jpeg"
+                }
+            ]
+        },
+        {
+            id: 8,
+            nombre: "Ferrari 488 GTB",
+            descripcion: "488 GTB 2022 5.000km",
+            foto: "foto8",
+            comentarios: [
+                {
+                    nombre: "Enzo",
+                    comentario: "El sueño italiano",
+                    imagen_perfil: "/images/users/enzodiaz.png"
+                },
+                {
+                    nombre: "borja",
+                    comentario: "La pasión de Ferrari",
+                    imagen_perfil: "/images/users/borja.png"
+                },
+                {
+                    nombre: "nacho",
+                    comentario: "Una obra maestra de ingeniería",
+                    imagen_perfil: "/images/users/ponzio.jpeg"
+                }
+            ]
+        },
+        {
+            id: 9,
+            nombre: "Lamborghini Huracán",
+            descripcion: "Huracán EVO 2023 7.000km",
+            foto: "foto9",
+            comentarios: [
+                {
+                    nombre: "Enzo",
+                    comentario: "Puro poder italiano",
+                    imagen_perfil: "/images/users/enzodiaz.png"
+                },
+                {
+                    nombre: "borja",
+                    comentario: "Lamborghini es arte en movimiento",
+                    imagen_perfil: "/images/users/borja.png"
+                },
+                {
+                    nombre: "nacho",
+                    comentario: "Emoción pura al volante",
+                    imagen_perfil: "/images/users/ponzio.jpeg"
+                }
+            ]
+        },
+        {
+            id: 10,
+            nombre: "McLaren 720S",
+            descripcion: "720S Spider 2022 3.000km",
+            foto: "foto10",
+            comentarios: [
+                {
+                    nombre: "Enzo",
+                    comentario: "Diseño impresionante",
+                    imagen_perfil: "/images/users/enzodiaz.png"
+                },
+                {
+                    nombre: "borja",
+                    comentario: "Innovación y rendimiento",
+                    imagen_perfil: "/images/users/borja.png"
+                },
+                {
+                    nombre: "nacho",
+                    comentario: "Experiencia de conducción única",
+                    imagen_perfil: "/images/users/ponzio.jpeg"
+                }
+            ]
+        }
+    ]
 }
