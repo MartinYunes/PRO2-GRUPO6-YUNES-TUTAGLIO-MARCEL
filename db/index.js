95096c6bf990d28b855e38f032ed611000e9c9e6
Juan Tutaglio <JuanTutaglio@MacBook-Pro-de-Mauge.local>
2024-03-25 09:57:53 -0300

    boton submit

---
+++ db/index.js
@@ -0,0 +1,12 @@
+let datos = {
+    usuario:{
+    id: 1,
+    email: "hola@gmail.com",
+    usuario: "hola",
+    contraseña: "hola",
+    fecha_nacimiento: 22/02/2024,
+    documento: 12345678,
+    foto: "/images/users/default-image.png",
+    },
+
+}
