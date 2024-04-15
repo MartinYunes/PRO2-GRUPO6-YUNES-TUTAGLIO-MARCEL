3198b37dffefd65c8c26929203882b88bca949d8
Thiago Marcel <thiagomarcel@Thiagos-MacBook-Pro.local>
2024-04-15 09:15:39 -0300

    vistas de resultados

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -25,7 +25,25 @@ let indexController = {
       },
 
     searchResults: function(req, res, next) {
-        res.render('search-results', { title: 'Express' });
+      let titulo_auto = []
+      let descripcion_auto = []
+      let comentarios = []
+      let imagenes = []
+      let id = []
+      for (let i = 0; i < autos.productos.length; i++) {
+        titulo_auto.push(autos.productos[i].nombre)
+        descripcion_auto.push(autos.productos[i].descripcion)
+        comentarios.push(autos.productos[i].comentarios)
+        imagenes.push(autos.productos[i].foto)
+        id.push(autos.productos[i].id)
+      }
+
+        res.render('search-results', { title: titulo_auto, 
+        descripcion: descripcion_auto,
+        comentarios : comentarios,
+        imagen : imagenes,
+        id : id
+        });
       },
 
 }
