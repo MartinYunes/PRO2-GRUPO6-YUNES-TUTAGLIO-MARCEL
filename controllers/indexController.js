9608be164fd4c6aa2a978ea2bec34353085e272b
Juan Tutaglio <JuanTutaglio@MacBook-Pro-de-Mauge.local>
2024-05-14 14:42:12 -0300

    correcion, SQL y conexiones

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,18 +3,19 @@ const db = require("../database/models")
 
 let indexController = {
     index: function(req, res, next) {
-      let titulo_auto = []
-      let descripcion_auto = []
-      let comentarios = []
-      let imagenes = []
-      let id = []
-      for (let i = 0; i < autos.productos.length; i++) {
-        titulo_auto.push(autos.productos[i].nombre)
-        descripcion_auto.push(autos.productos[i].descripcion)
-        comentarios.push(autos.productos[i].comentarios)
-        imagenes.push(autos.productos[i].foto)
-        id.push(autos.productos[i].id)
-      }
+
+      // let titulo_auto = []
+      // let descripcion_auto = []
+      // let comentarios = []
+      // let imagenes = []
+      // let id = []
+      // for (let i = 0; i < autos.productos.length; i++) {
+      //   titulo_auto.push(autos.productos[i].nombre)
+      //   descripcion_auto.push(autos.productos[i].descripcion)
+      //   comentarios.push(autos.productos[i].comentarios)
+      //   imagenes.push(autos.productos[i].foto)
+      //   id.push(autos.productos[i].id)
+      // }
 
        // res.render('index', { title: titulo_auto, 
         //descripcion: descripcion_auto,
@@ -22,9 +23,29 @@ let indexController = {
        // imagen : imagenes,
        // id : id
       //  });
-    db.Usuario.findAll().then(function (result) {
-      
-      return res.send(result)
+
+
+    db.Producto.findAll().then(function (result) {
+      let titulo_auto = []
+      let descripcion_auto = []
+      let comentarios = []
+      let imagenes = []
+      let id = []
+      console.log(imagenes);
+      for (let i = 0; i < result.length; i++) {
+        titulo_auto.push(result[i].nombreProducto)
+        descripcion_auto.push(result[i].descripcion)
+        comentarios.push(autos.productos[i].comentarios)
+        imagenes.push(result[i].imagen)
+        id.push(result[i].id)
+      }
+
+      return res.render('index', { title: titulo_auto, 
+        descripcion: descripcion_auto,
+        comentarios : comentarios,
+       imagen : imagenes,
+       id : id
+       });
     }).catch(error=>console.log(error)) 
     },
 
