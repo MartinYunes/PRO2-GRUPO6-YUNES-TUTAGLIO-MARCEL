cd941c2c454da869267873e112e4d0373331ee68
Thiago Marcel <thiagomarcel@Thiagos-MacBook-Pro.local>
2024-06-03 11:46:25 -0300

    punto 8 3/4

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const {body} = require("express-validator")
+let validation = [
+    body("imagen")
+    .notEmpty().withMessage("Debes ingresar imagen").bail(),
+
+    body("nombreProducto")
+    .notEmpty().withMessage("Debes ingresar un nombre del producto").bail(),
+
+    body("descripción")
+    .notEmpty().withMessage("Debes ingresar una descripcion").bail()
+]
 
 let productController = require('../controllers/productController')
 
 
-router.get('/add', productController.add);
+router.get('/add', validation, productController.add);
 
 router.get('/:id', productController.productos);
 
